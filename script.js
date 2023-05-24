cc985d1ace6adb3828f011f158abe9ff4933aaee
Ajmal777 <mdajmal6979@gmail.com>
2023-05-24 20:04:48 +0530

    Changed the grid size and added few features

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.body.querySelector(".grid");
-for(let i=0; i<800; i++){
+document.getElementById("header").innerHTML = "Hover to change color, click to show Hex code";
+
+for(let i=0; i<680; i++){
     const square = document.createElement("div");
     square.className = "square";
     grid.appendChild(square);
@@ -7,16 +9,27 @@ for(let i=0; i<800; i++){
   
 const squareArray = document.querySelectorAll(".square");
 squareArray.forEach((cell) => {
+    const color = getColor();
+    cell.addEventListener('click', ()=>{
+        document.getElementById("header").innerHTML = color;
+    })
     cell.addEventListener('mouseover', () =>{
-        const red = Math.floor(Math.random() * 255);
-        const blue = Math.floor(Math.random() * 255);
-        const green = Math.floor(Math.random() * 255);
-        const color = `rgb(${red}, ${green}, ${blue})`;
         cell.style.backgroundColor = color;
         document.getElementById("header").style.color = color;
     });
 
     cell.addEventListener('mouseout', () =>{
+        document.getElementById("header").innerHTML = 'Grid';
         cell.style.backgroundColor = '';
     })
 })
+
+function getColor(){
+    const hex = "0123456789ABCDEF"
+    let hexCode = "#";
+    for(let i=0; i<6; i++){
+        const random = Math.floor(Math.random() * hex.length);
+        hexCode += hex[random];
+    }
+    return hexCode;
+}
